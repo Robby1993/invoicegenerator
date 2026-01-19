aa3a516ad88ffea52bc3e828a3af3351ae6d0f53
robinson <robinsonmacwan93@gmail.com>
2026-01-19 23:34:02 +0530

    package name update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.robinson.invoicegenerator"
+    namespace = "com.robbym.invoicegenerator"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.robinson.invoicegenerator"
+        applicationId = "com.robbym.invoicegenerator"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/robinson/invoicegenerator/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.robinson.invoicegenerator
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
