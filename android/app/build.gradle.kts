6291c420e8bdddf08543952ced7430e7652d3538
robinson <robinsonmacwan93@gmail.com>
2026-02-03 14:54:40 +0530

    price update gst percent dynamic

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.robbym.invoicegenerator"
+            namespace = "com.robbym.invoicegenerator"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
