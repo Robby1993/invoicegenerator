fcb484be0acd008d3d7e9771cbc426c6f49f925e
robinson <robinsonmacwan93@gmail.com>
2026-05-26 22:40:07 +0530

    update pdf logo asset path

--- lib/utils/pdf_generator.dart
+++ lib/utils/pdf_generator.dart
@@ -10,7 +10,7 @@ class PDFGenerator {
     final pdf = pw.Document();
 
     final Uint8List logoBytes = (await rootBundle.load(
-      'assets/icon/logo.png',
+      'assets/icon/app_icon.png',
     )).buffer.asUint8List();
 
     final logo = pw.MemoryImage(logoBytes);
